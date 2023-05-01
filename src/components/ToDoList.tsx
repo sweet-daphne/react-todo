2a7713eb1d952fc95cb660e86167bb8d12498890
suelrin <dancerelrin@gmail.com>
2023-05-01 13:56:29 +0900

    add toDo

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-
 /*function ToDoList() {
   const [toDo, setTodo] = useState('');
   const [toDoError, setTodoError] = useState('');
@@ -29,7 +26,7 @@ import { useForm } from 'react-hook-form';
   );
 }*/
 
-interface IForm {
+/*interface IForm {
   email: string;
   firstName: string;
   lastName: string;
@@ -120,6 +117,56 @@ function ToDoList() {
         <button>Add</button>
         <span>{errors?.extreaError?.message}</span>
       </form>
+    </div>
+  );
+}*/
+import { useForm } from 'react-hook-form';
+import { atom, useRecoilState } from 'recoil';
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDo {
+  text: string;
+  id: number;
+  category: 'TO_DO' | 'DOING' | 'DONE';
+}
+
+const toDoState = atom<IToDo[]>({
+  key: 'toDo',
+  default: [],
+});
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: 'TO_DO' },
+      ...oldToDos,
+    ]);
+    setValue('toDo', '');
+  };
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register('toDo', {
+            required: 'Please write a To Do',
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
