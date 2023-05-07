d7ea6491a72b00d7316bba9aec131132641f0754
suelrin <dancerelrin@gmail.com>
2023-05-08 06:48:39 +0900

    enum

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,14 +1,20 @@
 import { atom, selector } from 'recoil';
 
+export enum Categories {
+  'TO_DO' = 'TO_DO',
+  'DOING' = 'DOING',
+  'DONE' = 'DONE',
+}
+
 export interface IToDo {
   text: string;
   id: number;
-  category: 'TO_DO' | 'DOING' | 'DONE';
+  category: Categories;
 }
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: 'category',
-  default: 'TO_DO',
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
@@ -21,7 +27,6 @@ export const toDoSelector = selector({
   get: ({ get }) => {
     const toDos = get(toDoState);
     const category = get(categoryState);
-    console.log(category);
     return toDos.filter((toDo) => toDo.category === category);
   },
 });
